69a125810aaf6e2c59c2da3a614ffb65167a498c
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2022-04-23 13:52:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './Components/FirstComponent';
 
 function App() {
+  const menu = [<li>un</li>, <li>deux</li>, <li>trois</li>];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+             <FirstComponent />
+          <ul> {menu}
+          </ul>
+       
       </header>
     </div>
   );
---
+++ src/Components/FirstComponent.js
@@ -0,0 +1,11 @@
+
+
+
+export default function FirstComponent() {
+    const name = "Foulen";
+    const name1 = <p> Ben foulen </p>;
+    return ( <div>
+        {name1}
+        <p> hello {name} </p>
+        </div>)
+}
